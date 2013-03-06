658ec7eb0a42f9d55d62ad6f554ca1e8dbd74762
Christian Malmos <chriller855@gmail.com>
2013-03-06 10:11:07 +0100

    tilføjet playerlayout

---
+++ playerlayout/playerlayout/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using playerlayout.Properties;
+
+namespace playerlayout
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool play = new bool();
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            
+        }
+
+        private void ButtonX_OnClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void ButtonM_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Full Skærm");
+        }
+        private void ButtonMini_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("minimize window");
+        }
+
+        private void Playbutton_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (play)
+            {
+                pap.Source = new BitmapImage(new Uri("play.png", UriKind.Relative));
+                play = false;
+            }
+
+
+            else
+            {
+                pap.Source = new BitmapImage(new Uri("Pause.png", UriKind.Relative));
+                play = true;
+            }
+            
+            
+        }
+
+
+        
+    }
+}
